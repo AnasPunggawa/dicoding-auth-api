6c0cebf6b2b12c06bbd489c3438f1fffe10e117a
Anas Punggawa <80744287+AnasPunggawa@users.noreply.github.com>
2025-06-02 21:03:00 +0800

    Revert "add say hello world test case"

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -29,24 +29,6 @@ describe('HTTP server', () => {
     expect(response.statusCode).toEqual(404);
   });
 
-  describe('when GET /', () => {
-    it('should return 200 and hello world', async () => {
-      // Arrange
-      const server = await createServer({});
-
-      // Action
-      const response = await server.inject({
-        method: 'GET',
-        url: '/',
-      });
-
-      // Assert
-      const responseJson = JSON.parse(response.payload);
-      expect(response.statusCode).toEqual(200);
-      expect(responseJson.value).toEqual('Hello World!');
-    });
-  });
-
   describe('when POST /users', () => {
     it('should response 201 and persisted user', async () => {
       // Arrange
@@ -90,9 +72,7 @@ describe('HTTP server', () => {
       const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toEqual(400);
       expect(responseJson.status).toEqual('fail');
-      expect(responseJson.message).toEqual(
-        'tidak dapat membuat user baru karena properti yang dibutuhkan tidak ada'
-      );
+      expect(responseJson.message).toEqual('tidak dapat membuat user baru karena properti yang dibutuhkan tidak ada');
     });
 
     it('should response 400 when request payload not meet data type specification', async () => {
@@ -115,9 +95,7 @@ describe('HTTP server', () => {
       const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toEqual(400);
       expect(responseJson.status).toEqual('fail');
-      expect(responseJson.message).toEqual(
-        'tidak dapat membuat user baru karena tipe data tidak sesuai'
-      );
+      expect(responseJson.message).toEqual('tidak dapat membuat user baru karena tipe data tidak sesuai');
     });
 
     it('should response 400 when username more than 50 character', async () => {
@@ -140,9 +118,7 @@ describe('HTTP server', () => {
       const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toEqual(400);
       expect(responseJson.status).toEqual('fail');
-      expect(responseJson.message).toEqual(
-        'tidak dapat membuat user baru karena karakter username melebihi batas limit'
-      );
+      expect(responseJson.message).toEqual('tidak dapat membuat user baru karena karakter username melebihi batas limit');
     });
 
     it('should response 400 when username contain restricted character', async () => {
@@ -165,9 +141,7 @@ describe('HTTP server', () => {
       const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toEqual(400);
       expect(responseJson.status).toEqual('fail');
-      expect(responseJson.message).toEqual(
-        'tidak dapat membuat user baru karena username mengandung karakter terlarang'
-      );
+      expect(responseJson.message).toEqual('tidak dapat membuat user baru karena username mengandung karakter terlarang');
     });
 
     it('should response 400 when username unavailable', async () => {
@@ -280,9 +254,7 @@ describe('HTTP server', () => {
       const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toEqual(401);
       expect(responseJson.status).toEqual('fail');
-      expect(responseJson.message).toEqual(
-        'kredensial yang Anda masukkan salah'
-      );
+      expect(responseJson.message).toEqual('kredensial yang Anda masukkan salah');
     });
 
     it('should response 400 if login payload not contain needed property', async () => {
@@ -303,9 +275,7 @@ describe('HTTP server', () => {
       const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toEqual(400);
       expect(responseJson.status).toEqual('fail');
-      expect(responseJson.message).toEqual(
-        'harus mengirimkan username dan password'
-      );
+      expect(responseJson.message).toEqual('harus mengirimkan username dan password');
     });
 
     it('should response 400 if login payload wrong data type', async () => {
@@ -327,9 +297,7 @@ describe('HTTP server', () => {
       const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toEqual(400);
       expect(responseJson.status).toEqual('fail');
-      expect(responseJson.message).toEqual(
-        'username dan password harus string'
-      );
+      expect(responseJson.message).toEqual('username dan password harus string');
     });
   });
 
@@ -356,9 +324,7 @@ describe('HTTP server', () => {
           password: 'secret',
         },
       });
-      const {
-        data: { refreshToken },
-      } = JSON.parse(loginResponse.payload);
+      const { data: { refreshToken } } = JSON.parse(loginResponse.payload);
 
       // Action
       const response = await server.inject({
@@ -434,9 +400,7 @@ describe('HTTP server', () => {
     it('should return 400 if refresh token not registered in database', async () => {
       // Arrange
       const server = await createServer(container);
-      const refreshToken = await container
-        .getInstance(AuthenticationTokenManager.name)
-        .createRefreshToken({ username: 'dicoding' });
+      const refreshToken = await container.getInstance(AuthenticationTokenManager.name).createRefreshToken({ username: 'dicoding' });
 
       // Action
       const response = await server.inject({
@@ -451,9 +415,7 @@ describe('HTTP server', () => {
       const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toEqual(400);
       expect(responseJson.status).toEqual('fail');
-      expect(responseJson.message).toEqual(
-        'refresh token tidak ditemukan di database'
-      );
+      expect(responseJson.message).toEqual('refresh token tidak ditemukan di database');
     });
   });
 
@@ -497,9 +459,7 @@ describe('HTTP server', () => {
       const responseJson = JSON.parse(response.payload);
       expect(response.statusCode).toEqual(400);
       expect(responseJson.status).toEqual('fail');
-      expect(responseJson.message).toEqual(
-        'refresh token tidak ditemukan di database'
-      );
+      expect(responseJson.message).toEqual('refresh token tidak ditemukan di database');
     });
 
     it('should response 400 if payload not contain refresh token', async () => {
